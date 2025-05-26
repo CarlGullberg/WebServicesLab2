41aa57fc0ca3eb7053cd55495824f43663bada07
Anton <antongrubisic@outlook.com>
2025-05-26 19:16:20 +0200

    Fixed so getting code 200 while accessing with token, 401 without

---
+++ src/main/java/se/iths/jokenation/JokenationApplication.java
@@ -0,0 +1,13 @@
+package se.iths.jokenation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokenationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokenationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/jokenation/JokenationApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.jokenation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokenationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
